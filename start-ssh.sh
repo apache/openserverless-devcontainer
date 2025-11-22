aeff74190ae7595faff5448efe7e1b475371e7ff
Michele Sciabarra <msciabarra@apache.org>
2025-11-22 11:39:38 +0000

    image

--- start-ssh.sh
+++ start-ssh.sh
@@ -2,9 +2,15 @@
 sudo mkdir -p /run/sshd
 sudo ssh-keygen -A
 mkdir -p ~/.ssh
-chmod 600 ~/.ssh/authorized_keys
+PORT=${SSH_PORT:-2222} d
 if test -n "$AUTHORIZED_KEY"
 then echo "$AUTHORIZED_KEY" >>~/.ssh/authorized_keys
+     chmod 600 ~/.ssh/authorized_keys
 fi
-ln -sf /workspace ~/workspace
-sudo chown -R 1000:1000 /workspace
+if test -d /workspace
+then
+    ln -sf /workspace ~/workspace
+    sudo chown -R 1000:1000 /workspace
+fi
+echo Starting ssh in port $PORT
+sudo /usr/sbin/sshd -p $PORT -D
